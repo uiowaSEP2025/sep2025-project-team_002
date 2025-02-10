9384d27b1dff05670a0d217021ec37757cf9cb94
Anna Davis <anna-davis@uiowa.edu>
2025-02-10 17:00:31 -0600

    removing unnecessary backend files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.home',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
