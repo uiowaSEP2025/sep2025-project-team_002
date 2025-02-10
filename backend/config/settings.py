581cfc4d5be38a732a9ff5c4faaba8fac613f9b2
rmdna <rodrigo-medina@uiowa.edu>
2025-02-10 12:01:59 -0600

    Added home to the INSTALLED_APPS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.home',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
