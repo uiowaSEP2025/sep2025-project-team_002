a7d5cff7caa17a2b9a9fe9c038f8680433bb71bf
rmdna <rodrigo-medina@uiowa.edu>
2025-02-14 12:09:31 -0600

    Added test for sign up creation

--- backend/users/tests/test_signup.py
+++ backend/users/tests/test_signup.py
@@ -1,4 +1,29 @@
 # This will be the backend testing file for Signup
+# Looking to test:
+# Duplicates, Regular, Email unique, wrong verify password
 
 import pytest
 import requests
+
+
+@pytest.mark.django_db
+def test_signup_creation(live_server):
+    url = f"{live_server.url}/users/signup/"
+    data = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "email": "john@example.com",
+        "password": "Password123",
+        "verifyPassword": "Password123",
+        "transferType": "transfer_in",
+    }
+    response = requests.post(url, json=data)
+    print(response.json())
+
+    # Expecting 201 for a successful creation
+    assert response.status_code == 201
+    json_response = response.json()
+    # Check for a key that exists in your response (e.g., 'id')
+    assert "id" in json_response
+    # You can also check that the email is as expected
+    assert json_response["email"] == "john@example.com"
