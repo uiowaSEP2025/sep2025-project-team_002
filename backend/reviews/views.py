3807f8577c7a354274ef5f23b11699e911b23145
Anna Davis <anna-davis@uiowa.edu>
2025-02-17 15:43:13 -0600

    added submit confirmation and disabled submit if coach,sport,school have been submitted by that user before

--- backend/reviews/views.py
+++ backend/reviews/views.py
@@ -9,3 +9,10 @@ class CreateReviewView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)  # Assign logged-in user
+
+class UserReviewsView(generics.ListAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Require JWT authentication
+
+    def get_queryset(self):
+        return Reviews.objects.filter(user=self.request.user)
