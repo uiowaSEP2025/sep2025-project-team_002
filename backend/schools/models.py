2f7be2252d679d7ed20f08ddd416b0dadbf76522
samanthapoth <samantha-pothitakis@uiowa.edu>
2025-02-16 21:57:02 -0600

    updated model with correct name reference

--- backend/schools/models.py
+++ backend/schools/models.py
@@ -22,4 +22,4 @@ class Schools(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.name} - {self.conference}"
+        return f"{self.school_name} - {self.conference}"
