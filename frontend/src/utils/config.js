bb815b23b4ebdc18af520a60fe48dfaf40a229df
mirrorigin <jingmliang@uiowa.edu>
2025-02-20 15:55:03 -0600

    Final Adjust

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,6 +1,9 @@
 const API_BASE_URL =
   import.meta.env.MODE === "test"
     ? "http://backend:8000"
-    : import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+    : import.meta.env.VITE_API_BASE_URL;
+
+console.log("API_BASE_URL:", API_BASE_URL);
+console.log("VITE_API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
 
 export default API_BASE_URL;
