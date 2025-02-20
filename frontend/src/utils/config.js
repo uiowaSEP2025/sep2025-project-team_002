cbc4673b42557861f606bb3cbea7b4c92af2ee67
mirrorigin <jingmliang@uiowa.edu>
2025-02-20 10:26:57 -0600

    Fixed API Configuration

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,6 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL =
+  import.meta.env.MODE === "test"
+    ? "http://backend:8000"
+    : import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+
 export default API_BASE_URL;
--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -2,16 +2,20 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+  // Load app-level env vars to node-level env vars.
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
+    define: {
+      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(env.VITE_API_BASE_URL),
+    },
     server: {
       host: "0.0.0.0",
       port: 3000,
       strictPort: true,
       proxy: {
         "/api": {
-          target: env.VITE_API_BASE_URL,
+          target: process.env.VITE_API_BASE_URL,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, "")
         }
