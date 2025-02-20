caefd9c6d980201a854318491a0e5380f3c2162a
mirrorigin <jingmliang@uiowa.edu>
2025-02-20 01:41:41 -0600

    React App works

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,5 +1,2 @@
-const API_BASE_URL = window.location.hostname === "localhost"
-  ? "http://localhost:8000"
-  : "https://theathleticinsider.com";
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export default API_BASE_URL;
---
+++ frontend/vite.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    server: {
+      host: "0.0.0.0",
+      port: 3000,
+      strictPort: true,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, "")
+        }
+      }
+    }
+  };
+});
