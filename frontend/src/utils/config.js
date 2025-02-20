a5a4cccef6de51544fc222bc0a4e46f9eadbde43
mirrorigin <jingmliang@uiowa.edu>
2025-02-20 16:16:14 -0600

    Final Version

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -3,7 +3,7 @@ const API_BASE_URL =
     ? "http://backend:8000"
     : import.meta.env.VITE_API_BASE_URL;
 
-console.log("API_BASE_URL:", API_BASE_URL);
-console.log("VITE_API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
+console.log("API Config Debugging... API_BASE_URL:", API_BASE_URL);
+console.log("API Config Debugging... VITE_API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
 
 export default API_BASE_URL;
