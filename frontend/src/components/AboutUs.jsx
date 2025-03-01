05b17577e10e072806a979e32bf660e1e0157944
Anna Davis <anna-davis@uiowa.edu>
2025-03-01 14:41:34 -0600

    Added details to the about us page

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -41,7 +41,25 @@ function AboutUs() {
     <div style={{ maxWidth: '900px', margin: 'auto', padding: '20px', backgroundColor: "#f5f5f5" }}>
 
       <h1>Who we are!</h1>
-      <h2>Welcome to Athletic Insider, your go-to platform for the latest in sports transfer insights.</h2>
+      <h2>Your go-to platform for the latest in sports transfer insights.</h2>
+        <p>We’re a team of five passionate students from the University of Iowa who came together with a shared mission:
+            to make the student-athlete transfer process smoother, more transparent, and fair.
+            Our journey started in our Software Engineering Projects course, where we realized we had the desire to create something meaningful. </p>
+
+        <p>The spark for this project came from Anna, a former D1 volleyball player who experienced
+            firsthand how challenging and unclear the transfer process can be. She brought the idea to the group,
+            and from that moment, we were all in. We saw the real impact our platform could have on thousands of
+            student-athletes navigating big changes in their careers and lives. Now, we're turning that vision into
+            reality—one line of code at a time! </p>
+
+        <h2>Meet our team:</h2>
+        <p><b>Samantha Pothitakis:</b> Electrical and Computer Engineering, MS student </p>
+        <p><b>Rodrigo Medina:</b> Computer Science, BA student </p>
+        <p><b>Jingming Liang:</b> PhD student in Electrical and Computer Engineering</p>
+        <p><b>Yusuf Halim:</b> Electrical and Computer Engineering, BSE student</p>
+        <p><b>Anna Davis:</b> Master of Computer Science student</p>
+
+        <p>WE WILLA ADD A TEAM IMAGE ON THIS PAGE TOO</p>
     </div>
           </>
   );
