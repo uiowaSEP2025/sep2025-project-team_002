b699e1090fd10b7dac2fa5cdc3476f5c23c3cad7
Sam Pothitakis <samantha-pothitakis@uiowa.edu>
2025-03-03 13:26:05 -0600

    updated to Schools

--- backend/schools/views.py
+++ backend/schools/views.py
@@ -1,26 +1,26 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from .models import School
+from .models import Schools
 from .serializers import SchoolSerializer
 
 # Public views
 class SchoolListView(generics.ListAPIView):
-    queryset = School.objects.all()
+    queryset = Schools.objects.all()
     serializer_class = SchoolSerializer
     permission_classes = [AllowAny]
 
 class SchoolDetailView(generics.RetrieveAPIView):
-    queryset = School.objects.all()
+    queryset = Schools.objects.all()
     serializer_class = SchoolSerializer
     permission_classes = [AllowAny]
 
 # Protected views
 class ProtectedSchoolListView(generics.ListCreateAPIView):
-    queryset = School.objects.all()
+    queryset = Schools.objects.all()
     serializer_class = SchoolSerializer
     permission_classes = [IsAuthenticated]
 
 class ProtectedSchoolDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = School.objects.all()
+    queryset = Schools.objects.all()
     serializer_class = SchoolSerializer
     permission_classes = [IsAuthenticated]
