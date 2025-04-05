7ce6fd14eb8403a005629db4127f3d3e6a1b02ea
Yusuf Halim <yusuf-halim@uiowa.edu>
2025-04-05 16:56:21 -0500

    Added selenium tests to cover both Account.jsx & AccountSettings.jsx

--- frontend/src/tests/selenium/005_account_page.test.js
+++ frontend/src/tests/selenium/005_account_page.test.js
@@ -1,4 +1,4 @@
-// 005_account_page.test.js
+/* eslint-env node, mocha */
 import { Builder, By, until } from "selenium-webdriver";
 import { expect } from "chai";
 import { describe, it, before, after } from "mocha";
